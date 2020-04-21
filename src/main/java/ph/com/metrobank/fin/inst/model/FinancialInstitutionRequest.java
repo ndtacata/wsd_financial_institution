c8bbb18ad0d2b5de5ea709944a890e39e08fb4a1
Jairus Belza <jairus.belza@gmail.com>
2020-04-21 11:20:32 +0800

    Resolve Sonarlint issue on code duplication

--- src/main/java/ph/com/metrobank/fin/inst/model/FinancialInstitutionRequest.java
+++ src/main/java/ph/com/metrobank/fin/inst/model/FinancialInstitutionRequest.java
@@ -13,71 +13,92 @@ public class FinancialInstitutionRequest {
 	private int enrollmentEnabled;
 	private String createdBy;
 	private String updatedBy;
-	
+
 	public String getName() {
 		return name;
 	}
-	public String getBrstn() {
-		return brstn;
-	}
-	public String getSwiftCode() {
-		return swiftCode;
-	}
-	public String getAddress() {
-		return address;
-	}
-	public String getBancnetCode() {
-		return bancnetCode;
-	}
-	public String getPchcCode() {
-		return pchcCode;
-	}
-	public int getTransferEnabled() {
-		return transferEnabled;
-	}
-	public int getEnrollmentEnabled() {
-		return enrollmentEnabled;
-	}
-	public String getCreatedBy() {
-		return createdBy;
-	}
-	public String getUpdatedBy() {
-		return updatedBy;
-	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	public void setBrstn(String brstn) {
-		this.brstn = brstn;
-	}
-	public void setSwiftCode(String swiftCode) {
-		this.swiftCode = swiftCode;
-	}
-	public void setAddress(String address) {
-		this.address = address;
-	}
-	public void setBancnetCode(String bancnetCode) {
-		this.bancnetCode = bancnetCode;
-	}
-	public void setPchcCode(String pchcCode) {
-		this.pchcCode = pchcCode;
-	}
-	public void setTransferEnabled(int transferEnabled) {
-		this.transferEnabled = transferEnabled;
-	}
-	public void setEnrollmentEnabled(int enrollmentEnabled) {
-		this.enrollmentEnabled = enrollmentEnabled;
-	}
-	public void setCreatedBy(String createdBy) {
-		this.createdBy = createdBy;
-	}
-	public void setUpdatedBy(String updatedBy) {
-		this.updatedBy = updatedBy;
-	}
+
 	public String getType() {
 		return type;
 	}
+
 	public void setType(String type) {
 		this.type = type;
+	}
+
+	public String getBrstn() {
+		return brstn;
+	}
+
+	public void setBrstn(String brstn) {
+		this.brstn = brstn;
+	}
+
+	public String getSwiftCode() {
+		return swiftCode;
+	}
+
+	public void setSwiftCode(String swiftCode) {
+		this.swiftCode = swiftCode;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getBancnetCode() {
+		return bancnetCode;
+	}
+
+	public void setBancnetCode(String bancnetCode) {
+		this.bancnetCode = bancnetCode;
+	}
+
+	public String getPchcCode() {
+		return pchcCode;
+	}
+
+	public void setPchcCode(String pchcCode) {
+		this.pchcCode = pchcCode;
+	}
+
+	public int getTransferEnabled() {
+		return transferEnabled;
+	}
+
+	public void setTransferEnabled(int transferEnabled) {
+		this.transferEnabled = transferEnabled;
+	}
+
+	public int getEnrollmentEnabled() {
+		return enrollmentEnabled;
+	}
+
+	public void setEnrollmentEnabled(int enrollmentEnabled) {
+		this.enrollmentEnabled = enrollmentEnabled;
+	}
+
+	public String getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public String getUpdatedBy() {
+		return updatedBy;
+	}
+
+	public void setUpdatedBy(String updatedBy) {
+		this.updatedBy = updatedBy;
 	}
 }
